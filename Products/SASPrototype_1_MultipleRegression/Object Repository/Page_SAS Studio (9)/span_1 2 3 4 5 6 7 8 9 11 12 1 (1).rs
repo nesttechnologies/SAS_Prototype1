357ced012b027ed543b35f72d0a9ebf81fff901e
Ganesh Krishnan Maha <ganesh.krishnanmaha@nesttech.com>
2017-10-06 12:35:22 -0400

    Add files via upload

---
+++ Products/SASPrototype_1_MultipleRegression/Object Repository/Page_SAS Studio (9)/span_1 2 3 4 5 6 7 8 9 11 12 1 (1).rs
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_1 2 3 4 5 6 7 8 9 11 12 1 (1)</name>
+   <tag></tag>
+   <elementGuidId>8530a9f2-948a-4e0f-833c-0cb2c2bfcd1c</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;perspectiveTabContainer_tabsBC_tab0_editor&quot;)/div[@class=&quot;textview sce sce-sas&quot;]/div[7]/div[@class=&quot;textviewContent&quot;]/div[4]/span[@class=&quot;annotationRange matchingSearch cards-data&quot;]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>contains</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>1 2 3 4 5 6 7 8 9 11 12 13 14 15 16 18 19 20 21 22 23 24 25 26 27 28 29 30 .</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>annotationRange matchingSearch cards-data</value>
+   </webElementProperties>
+</WebElementEntity>
